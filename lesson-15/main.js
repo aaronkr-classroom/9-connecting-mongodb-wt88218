b15a000ecde262e5d337721cba1bbaf54bc3b7d5
wt88218 <163230869+wt88218@users.noreply.github.com>
2024-05-08 04:59:34 +0000

    15끝

--- lesson-15/main.js
+++ lesson-15/main.js
@@ -18,7 +18,7 @@ const mongoose = require("mongoose"); // Mongoose 모듈의 요청
 mongoose.Promise = global.Promise; // 필요 없을 것 같은데?
 
 mongoose.connect(
-  "mongodb://localhost:27017/recipe_db", // 데이터베이스 연결 설정
+  "mongodb+srv://wt88218:gusxor0203@ut-node.blvb1gp.mongodb.net/?retryWrites=true&w=majority&appName=ut-node", // 데이터베이스 연결 설정
   { useNewUrlParser: true }
 );
 
@@ -57,16 +57,17 @@ app.get("/name/:myName", homeController.respondWithName2);
  * @TODO: Listing 15.2 (p. 216)
  * 구독자 컨트롤러 사용
  */
-
+app.get("/subscribers", subscribersController.getAllSubscribers2);
 
 /**
  * @TODO: Listing 15.5 (p. 219-220)
  * 구독을 위한 라우트
  */
 // 구독 페이지를 위한 GET 라우트
-
+app.get("/subscribe",subscribersController.getSubscriptionPage);
 // 구독 데이터 처리를 위한 POST 라우트
-
+app.post("/subscribe",subscribersController.saveSubscriber);
+//app.get("/subscribe", subscribersController.getAllSubscribers)
 
 /**
  * Listing 11.4 (p. 169)
